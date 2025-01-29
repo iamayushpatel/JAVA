c794e1a4fe47f026a0f21301db3942747c103d64
Ayush Patel <iamayushpatelap@gmail.com>
2025-01-29 21:23:34 +0530

    P-6

---
+++ P6_Add_Pattern.java
@@ -0,0 +1,155 @@
+public class P6_Add_Pattern {
+
+    public static void Pattern_1(int m,int n) {
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1 || i==m || j==1 || j==n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Pattern_2(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }    
+
+    public static void Pattern_3(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("1");
+                }else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Pattern_4(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }   
+    }
+
+    public static void Pattern_5(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Pattern_6(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                if(i==1 || i==n || j==1 || j==n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Pattern_7(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Pattern_8(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Pattern_9(int n) { 
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            for(int j=2;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        //Pattern_1(4,5);
+        //Pattern_2(5);
+        //Pattern_3(5);
+        //Pattern_4(4);
+        //Pattern_5(5);
+        //Pattern_6(5);
+        //Pattern_7(5);
+        //Pattern_8(5);
+        Pattern_9(5);
+    }
+}
