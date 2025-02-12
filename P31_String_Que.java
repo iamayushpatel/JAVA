55f0134b5ec2077d013b7ab7d3c03d80d3eab989
Ayush Patel <iamayushpatelap@gmail.com>
2025-02-12 21:10:12 +0530

    P-31

--- P31_String_Que.java
+++ P31_String_Que.java
@@ -14,7 +14,6 @@ public class P31_String_Que {
         System.out.println("Lower Case Vowels Is : " + count);
     }
 
-
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
          String str = sc.nextLine();
