14838be4a96546af3924f1411e16cbb35ddde4d9
Ayush Patel <iamayushpatelap@gmail.com>
2025-02-13 21:49:50 +0530

    P-32

--- P32_Bit_OddOrEven.java
+++ P32_Bit_OddOrEven.java
@@ -8,7 +8,6 @@ public class P32_Bit_OddOrEven {
             System.out.println("Odd");
         }
     }
-
     public static void main(String[] args) {
          oddOrEven(10);
          oddOrEven(13);
