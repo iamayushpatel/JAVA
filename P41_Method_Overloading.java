c577af711b034c05271608cb69c1c4751c0d44c7
Ayush Patel <iamayushpatelap@gmail.com>
2025-02-20 18:55:34 +0530

    P-41

---
+++ P41_Method_Overloading.java
@@ -0,0 +1,20 @@
+public class P41_Method_Overloading {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.sum(1, 2));
+        System.out.println(calc.sum(1.5f, 2.6f));
+        System.out.println(calc.sum(1, 2, 3));
+    }
+}
+
+class Calculator{
+    int sum(int a, int b){
+        return a+b;
+    }
+    float sum(float a, float b){
+        return a+b;
+    }
+    int sum(int a, int b, int c){
+        return a+b+c;
+    }
+}
