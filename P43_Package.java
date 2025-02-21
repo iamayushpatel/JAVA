ea19b1ffcd53098966797baa3a80ee53ec0c630a
Ayush Patel <iamayushpatelap@gmail.com>
2025-02-21 20:28:24 +0530

    P-43

---
+++ P43_Package.java
@@ -0,0 +1,9 @@
+import mypack.*;
+public class P43_Package {
+    public static void main(String[] args) {
+        Dell o1 = new Dell();
+        Hp o2 = new Hp();
+        o1.disp();
+        o2.disp();
+    }
+}
