e63098a593b3fc19cae56b0e68649c0d7b3136c1
Ayush Patel <iamayushpatelap@gmail.com>
2025-04-01 21:01:15 +0530

    P-67

---
+++ P67_Subset.java
@@ -0,0 +1,19 @@
+public class P67_Subset {
+    public static void findSubset(String str,String ans,int i){
+        if (i == str.length()) {
+            if(ans.length() == 0){
+                System.out.println("Null");
+            }else{
+                System.out.println(ans);
+            }
+            return ;
+        }
+        findSubset(str, ans+str.charAt(i), i+1);
+        findSubset(str, ans, i+1);
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        findSubset(str, "", 0);
+    }
+}
