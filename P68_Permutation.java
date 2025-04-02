421202cbbea445907c7e47014f46f9fdbc10fb47
Ayush Patel <iamayushpatelap@gmail.com>
2025-04-02 11:10:10 +0530

    P-68

---
+++ P68_Permutation.java
@@ -0,0 +1,18 @@
+public class P68_Permutation {
+    public static void permutation(String str,String ans){
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return ;
+        }
+        for(int i=0;i<str.length();i++){
+            char curr = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i+1); // delete i char
+            permutation(newStr, ans+curr);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        permutation(str, "");
+    }
+}
